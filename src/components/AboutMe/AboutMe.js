ab75c3dbdc8e06939f57965760b39416a9f9f509
Firsakov Dmitriy <firsakovds@mail.ru>
2023-08-11 01:38:15 +0300

    верстка блока aboutme

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,35 +7,34 @@ function AboutMe() {
     <section className="about-me" id="about-me">
       <h3 className="about-me__title section-title">Студент</h3>
       <div className="about-me__div">
-      <div className="about-me__info">
-        <h2 className="about-me__header section-header">Дмитрий</h2>
-        <p className="about-me__subtitle">Юный фронтенд-разработчик, 35 лет</p>
-        <p className="about-me__description">
-          Я родился в г. Калинин(так в паспорте написано), 17 июля 1990 года
-          Калинин был переименован в Тверь. Был в школе 11 классов, далее
-          университет ТГТУ(квалификация инженер). 10 лет программировал станки с
-          чпу, токарные фрезерные, лазерные, плазменные. Станки программировать
-          достаточно скучно, однообразно, поэтому решил обучиться на front-end
-          разработчика. Умею ездить на велосипеде и стрелять из лука,
-          одновременно(иногда даже попадаю в цель)
-        </p>
-        <Link
-          to="https://github.com/firsakovds"
-          className="about-me__link"
-          target="_blank"
-        >
-          Github
-        </Link>
-        
+        <div className="about-me__info">
+          <h2 className="about-me__header section-header">Дмитрий</h2>
+          <p className="about-me__subtitle">
+            Юный фронтенд-разработчик, 35 лет
+          </p>
+          <p className="about-me__description">
+            Я родился в г. Калинин(так в паспорте написано), 17 июля 1990 года
+            Калинин был переименован в Тверь. Был в школе 11 классов, далее
+            университет ТГТУ(квалификация инженер). 10 лет программировал станки
+            с чпу, токарные фрезерные, лазерные, плазменные. Станки
+            программировать достаточно скучно, однообразно, поэтому решил
+            обучиться на front-end разработчика. Умею ездить на велосипеде и
+            стрелять из лука, одновременно(иногда даже попадаю в цель)
+          </p>
+          <Link
+            to="https://github.com/firsakovds"
+            className="about-me__link"
+            target="_blank"
+          >
+            Github
+          </Link>
+        </div>
+        <img
+          className="about-me__foto"
+          src={MyFoto}
+          alt="Фотография юного студента"
+        />
       </div>
-
-      <img
-        className="about-me__foto"
-        src={MyFoto}
-        alt="Фотография юного студента"
-      />
-      </div>
-      
     </section>
   );
 }
